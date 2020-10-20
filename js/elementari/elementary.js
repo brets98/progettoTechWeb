652f61d261cfdfbfbf5756a218fd84f7375bb318
alessia vasalluccio <alessia.vasalluccio@studio.unibo.it>
2020-10-20 20:18:31 +0200

    aggiunta pagina 5 e 6 da aggiustare

--- js/elementari/elementary.js
+++ js/elementari/elementary.js
@@ -191,4 +191,279 @@
 //-----------------------------------4 FindMap--------------------------
     function fetchFindMap(){
       console.log("Carico trova mappa -> 4");
+      fetch("./json/dino/dino.json")
+      .then(function(res){
+      return res.json(); // pars the json in a regular js obj
+      }).then(function(obj){
+
+      pulisci();
+
+
+      appendDatamyData4(obj);
+      appendDataPicture4(obj);
+      appendDataButton4(obj);
+      console.log("Andato a buon fine")
+
+      }).catch(function(err){
+      console.error(err);
+      })
+
     }
+
+    function appendDatamyData4(obj){
+      var mainContainer = document.getElementById("myData");
+    for (var i = 0; i < obj.length; i++) {
+      var introduzione = document.createElement("p");
+      mainContainer.appendChild(introduzione);
+      introduzione.innerHTML = obj[i].FindMap.text;
+    }/*
+    console.log(introduzione);
+    console.log("----");*/
+
+  }
+
+
+  function appendDataPicture4(obj){
+      var picContainer = document.getElementById("picture");
+  
+      for (var j = 0; j < obj.length; j++) {
+        var urlvalue = JSON.stringify(obj[j].FindMap.img_url);
+        var stripped = urlvalue.replace(/['"]+/g, "");
+        var img_url = document.createElement("IMG");
+        img_url.setAttribute("src", stripped);
+        img_url.setAttribute("width", "640");
+        img_url.setAttribute("height", "220");
+        //img_url.setAttribute("alt", "The Pulpit Rock");
+        //document.body.appendChild(img_url);
+        picContainer.appendChild(img_url);
+        img_url.innerHTML = obj[j].FindMap.img_url;
+      }
+    /*
+      console.log(img_url);
+      console.log(urlvalue);
+      console.log(stripped);
+      console.log(picContainer);
+      console.log("----");*/
+    }
+    function appendDataButton4(obj){
+  
+      var pulsContainer = document.getElementById("pulsante");
+  
+      for (var i = 0; i < obj.length; i++) {
+        var urlvalue = JSON.stringify(obj[i].FindMap.botFunc);
+  
+        var stripped = urlvalue.replace(/['"]+/g, "");
+  
+        var puls = document.createElement("button");
+  
+        puls.setAttribute("onclick", stripped);
+  
+        pulsContainer.appendChild(puls);
+  
+        puls.innerHTML = obj[i].FindMap.botName;
+      }
+  /*
+      console.log(puls);
+      console.log(urlvalue);
+      console.log(stripped);
+      console.log(pulsContainer);*/
+  }
+
+  //-----------------------5 fetchPreareBackpack------------------//
+  function fetchPreareBackpack(){
+
+   console.log("Carico trova mappa -> 4");
+      fetch("./json/dino/dino.json")
+      .then(function(res){
+      return res.json(); // pars the json in a regular js obj
+      }).then(function(obj){
+
+      pulisci();
+
+
+      appendDatamyData5(obj);
+      appendDataPicture5(obj);
+      appendDataButton5(obj);
+      console.log("Andato a buon fine")
+
+      }).catch(function(err){
+      console.error(err);
+      })
+
+    }
+
+    function appendDatamyData5(obj){
+      var mainContainer = document.getElementById("myData");
+    for (var i = 0; i < obj.length; i++) {
+      var introduzione = document.createElement("p");
+      mainContainer.appendChild(introduzione);
+      introduzione.innerHTML = obj[i].PrepareBackpack.text;
+    }/*
+    console.log(introduzione);
+    console.log("----");*/
+
+  }
+
+
+  function appendDataPicture5(obj){
+      var picContainer = document.getElementById("picture");
+  
+      for (var j = 0; j < obj.length; j++) {
+        var urlvalue = JSON.stringify(obj[j].FindMap.img_url);
+        var stripped = urlvalue.replace(/['"]+/g, "");
+        var img_url = document.createElement("IMG");
+        img_url.setAttribute("src", stripped);
+        img_url.setAttribute("width", "640");
+        img_url.setAttribute("height", "220");
+        //img_url.setAttribute("alt", "The Pulpit Rock");
+        //document.body.appendChild(img_url);
+        picContainer.appendChild(img_url);
+        img_url.innerHTML = obj[j].PrepareBackpack.img_url;
+      }
+    /*
+      console.log(img_url);
+      console.log(urlvalue);
+      console.log(stripped);
+      console.log(picContainer);
+      console.log("----");*/
+    }
+    function appendDataButton5(obj){
+  
+      var pulsContainer = document.getElementById("pulsante");
+  
+      for (var i = 0; i < obj.length; i++) {
+        var urlvalue = JSON.stringify(obj[i].PrepareBackpack.botFunc);
+  
+        var stripped = urlvalue.replace(/['"]+/g, "");
+        var stripped2 = urlvalue.replace(/['"]+/g, "");
+        var stripped3 = urlvalue.replace(/['"]+/g, "");
+        var stripped4 = urlvalue.replace(/['"]+/g, "");
+        var stripped5 = urlvalue.replace(/['"]+/g, "");
+        var stripped6 = urlvalue.replace(/['"]+/g, "");
+
+  
+        var puls = document.createElement("button");
+        var puls2 = document.createElement("button");
+        var puls3 = document.createElement("button");
+        var puls4 = document.createElement("button");
+        var puls5 = document.createElement("button");
+        var puls6 = document.createElement("button");
+
+        puls.setAttribute("onclick", stripped);
+        puls2.setAttribute("onclick", stripped2);
+        puls3.setAttribute("onclick", stripped3);
+        puls4.setAttribute("onclick", stripped4);
+        puls5.setAttribute("onclick", stripped5);
+        puls6.setAttribute("onclick", stripped6);
+  
+        
+        pulsContainer.appendChild(puls);
+        pulsContainer2.appendChild(puls2);
+        pulsContainer3.appendChild(puls3);
+        pulsContainer4.appendChild(puls4);
+        pulsContainer5.appendChild(puls5);
+        pulsContainer6.appendChild(puls6);
+  
+        puls.innerHTML = obj[i].PrepareBackpack.botName;
+        puls2.innerHTML = obj[i].PrepareBackpack.botName2;
+        puls3.innerHTML = obj[i].PrepareBackpack.botName3;
+        puls4.innerHTML = obj[i].PrepareBackpack.botName4;
+        puls5.innerHTML = obj[i].PrepareBackpack.botName5;
+        puls6.innerHTML = obj[i].PrepareBackpack.botAvanti;
+
+
+      }
+  /*
+      console.log(puls);
+      console.log(urlvalue);
+      console.log(stripped);
+      console.log(pulsContainer);*/
+  }
+
+
+  //------------------PrepareBackpack--------------//
+
+  function addBottle(){
+    function fetchFindMap(){
+      console.log("Carico trova mappa -> 4");
+      fetch("./json/dino/dino.json")
+      .then(function(res){
+      return res.json(); // pars the json in a regular js obj
+      }).then(function(obj){
+
+      pulisci();
+
+
+      appendDatamyData6(obj);
+      appendDataPicture6(obj);
+      appendDataButton6(obj);
+      console.log("Andato a buon fine")
+
+      }).catch(function(err){
+      console.error(err);
+      })
+
+    }
+  }
+
+    function appendDatamyData6(obj){
+      var mainContainer = document.getElementById("myData");
+    for (var i = 0; i < obj.length; i++) {
+      var introduzione = document.createElement("p");
+      mainContainer.appendChild(introduzione);
+      introduzione.innerHTML = obj[i].FriendIntro.text;
+    }/*
+    console.log(introduzione);
+    console.log("----");*/
+
+  }
+
+
+  function appendDataPicture6(obj){
+      var picContainer = document.getElementById("picture");
+  
+      for (var j = 0; j < obj.length; j++) {
+        var urlvalue = JSON.stringify(obj[j].FriendIntro.img_url);
+        var stripped = urlvalue.replace(/['"]+/g, "");
+        var img_url = document.createElement("IMG");
+        img_url.setAttribute("src", stripped);
+        img_url.setAttribute("width", "640");
+        img_url.setAttribute("height", "220");
+        //img_url.setAttribute("alt", "The Pulpit Rock");
+        //document.body.appendChild(img_url);
+        picContainer.appendChild(img_url);
+        img_url.innerHTML = obj[j].FriendIntro.img_url;
+      }
+    /*
+      console.log(img_url);
+      console.log(urlvalue);
+      console.log(stripped);
+      console.log(picContainer);
+      console.log("----");*/
+    }
+    function appendDataButton6(obj){
+  
+      var pulsContainer = document.getElementById("pulsante");
+  
+      for (var i = 0; i < obj.length; i++) {
+        var urlvalue = JSON.stringify(obj[i].FriendIntro.botFunc);
+  
+        var stripped = urlvalue.replace(/['"]+/g, "");
+  
+        var puls = document.createElement("button");
+  
+        puls.setAttribute("onclick", stripped);
+  
+        pulsContainer.appendChild(puls);
+  
+        puls.innerHTML = obj[i].FriendIntro.botName;
+      }
+  /*
+      console.log(puls);
+      console.log(urlvalue);
+      console.log(stripped);
+      console.log(pulsContainer);*/
+  }
+
+
+  
